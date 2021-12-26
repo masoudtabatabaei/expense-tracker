a9da5d5388e604a43c2471aaa6323d60a6acf940
Masoud <masoudtabatabaei20@gmail.com>
2021-12-26 17:28:19 +0330

    implement Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <div style={{height:'80px' , lineHeight : '80px'}}>
+            <h2 style={{textAlign:'center'}}>Expense Tracker</h2>
+        </div>
+    );
+}
+
+export default Header;

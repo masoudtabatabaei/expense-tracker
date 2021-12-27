68731f008ae93681d131aabba04151df3414bcea
Masoud <masoudtabatabaei20@gmail.com>
2021-12-27 14:00:42 +0330

    implement TransactionList component

--- src/App.js
+++ src/App.js
@@ -1,33 +1,43 @@
-import Header from './components/Header';
-import Balance from './components/Balance';
-import IncomeExpenses from './components/IncomeExpenses';
+import Header from "./components/Header";
+import Balance from "./components/Balance";
+import IncomeExpenses from "./components/IncomeExpenses";
+import TransactionList from "./components/TransactionList";
 
-import './App.scss';
-
+import "./App.scss";
 
 function App() {
   return (
     <>
-      <Header/>
-      <div className='container'>
-        <div className='d-flex expense-container'>
-          <div className='expense__form'>
-            <Balance/>
-            <IncomeExpenses/>
-            <form>
+      <Header />
+      <div className="container">
+        <Balance />
+        <div className="d-flex expense-container">
+          <div className="expense__entry">
+            <IncomeExpenses />
+            <div className="header-title">Add new transaction</div>
+            <form method="POST">
               <label>
-                <div className='input-label'>Title</div>
-                <input className='input' type='text' placeholder='Enter title'/>
+                <div className="input-label">Title</div>
+                <input
+                  className="input"
+                  type="text"
+                  placeholder="Enter title"
+                />
               </label>
               <label>
-                <div className='input-label'>Amount</div>
-                <input className='input' type='text' placeholder='Enter amount'/>
+                <div className="input-label">Amount</div>
+                <input
+                  className="input"
+                  type="text"
+                  placeholder="Enter amount"
+                />
               </label>
-              <button className='fullwidth bold'>Add transaction</button>
+              <button className="fullwidth bold">Add transaction</button>
             </form>
           </div>
-          <div className='expense__history'>
-            ddd
+          <div className="expense__history">
+            <div className="header-title">History</div>
+            <TransactionList />
           </div>
         </div>
       </div>
---
+++ src/components/TransactionList.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import DeleteIcon from "../assets/img/delete.svg";
+
+const TransactionList = () => {
+  return (
+    <div className="inc-exp-items-container">
+      <div className="d-flex inc-item">
+        <div className="d-flex item--title">
+          <a>
+            <img src={DeleteIcon} />
+          </a>
+          <h4>Carwash</h4>
+        </div>
+        <div className="bold">+$6.00</div>
+      </div>
+      <div className="d-flex exp-item">
+        <div className="d-flex item--title">
+          <a>
+            <img src={DeleteIcon} />
+          </a>
+          <h4>Transfer</h4>
+        </div>
+        <div className="bold">-$6.00</div>
+      </div>
+    </div>
+  );
+};
+
+export default TransactionList;

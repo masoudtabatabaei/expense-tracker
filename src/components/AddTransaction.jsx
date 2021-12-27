ae0093cdaa21b904b46ce9d8b957fbd476193e6d
Masoud <masoudtabatabaei20@gmail.com>
2021-12-27 16:57:28 +0330

    create AddTransaction component

---
+++ src/components/AddTransaction.jsx
@@ -0,0 +1,37 @@
+import React , { useState } from "react";
+
+const AddTransaction = () => {
+  const [title, setTitle] = useState("");
+  const [amount, setAmount] = useState(0);
+
+  return (
+    <>
+      <div className="header-title">Add new transaction</div>
+      <form method="POST">
+        <label>
+          <div className="input-label">Title</div>
+          <input
+            className="input"
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            placeholder="Enter title"
+          />
+        </label>
+        <label>
+          <div className="input-label">Amount</div>
+          <input
+            className="input"
+            type="text"
+            value={amount}
+            onChange={(e) => setAmount(e.target.value)}
+            placeholder="Enter amount"
+          />
+        </label>
+        <button className="fullwidth bold">Add transaction</button>
+      </form>
+    </>
+  );
+};
+
+export default AddTransaction;
